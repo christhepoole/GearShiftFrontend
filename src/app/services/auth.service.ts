2a29d1a52368358b4e87b6e9d9a12efb6801212b
Chris Poole <christhepoole@gmail.com>
2022-09-30 14:09:59 -0400

    Tweaked the site routing a bit

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private baseURL: string = "https://localhost:7225/api/Login/";
+  private authChangeSubject = new Subject<boolean>();
+  public authChanged = this.authChangeSubject.asObservable();
 
   constructor(
     private http : HttpClient,
@@ -17,5 +20,9 @@ export class AuthService {
 
   public login(loginObj: any) {
     return this.http.post<any>(`${this.baseURL}login`, loginObj);
+  }
+
+  public sendAuthChangeNotification(isAuthenticated: boolean) {
+    this.authChangeSubject.next(isAuthenticated);
   }
 }
